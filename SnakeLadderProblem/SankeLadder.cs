efe16e5ed6b7de8cc97fccffd83c444d9e721050
sOny@4981 <18cse034.souravkumar@giet.edu>
2022-10-28 14:07:01 +0530

    [sourav]add : reapeatTill100-Uc4

--- SnakeLadderProblem/SankeLadder.cs
+++ SnakeLadderProblem/SankeLadder.cs
@@ -13,21 +13,29 @@ namespace SnakeLadderProblem
         //UC3
         public void GamePlay()
         {
-            int num = random.Next(1,7);
-            int check = random.Next(0,3);
-            switch (check)
+            while (position < 101)
             {
-                case 0:
-                    Console.WriteLine("Player is in the same position");
-                    break;
-                case 1:
-                    position += num;
-                    Console.WriteLine("Player position is : " + position);
-                    break;
-                case 2:
-                    position -= num;
-                    Console.WriteLine("Player position is : " + position);
-                    break;
+                int num = random.Next(1, 7);
+                int check = random.Next(0, 3);
+                switch (check)
+                {
+                    case 0:
+                        Console.WriteLine("Player is in the same position");
+                        break;
+                    case 1:
+                        position += num;
+                        Console.WriteLine("Player position is : " + position);
+                        break;
+                    case 2:
+                        position -= num;
+                        Console.WriteLine("Player position is : " + position);
+                        if(position < 0) 
+                        { 
+                            position = 0;
+                            Console.WriteLine("Player position change to 0 . So the player again Restart From O");
+                        }
+                        break;
+                }
             }
             
         }
