4fdf99feabe65565a462e80c419ae0123a439950
sOny@4981 <18cse034.souravkumar@giet.edu>
2022-10-28 13:30:16 +0530

    [sourav]add : Folder Structure

---
+++ SnakeLadderProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SnakeLadderProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welome to the Snake and Ladder Program");
+        }
+    }
+}
